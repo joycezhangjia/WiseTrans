15d07b050757418fa61476270af8d5cfe1445823
joycezhangjia <joycezhangjia@gmail.com>
2021-02-08 10:17:33 +0000

    training process

--- OfflineTraing/4step_traintestall.py
+++ OfflineTraing/4step_traintestall.py
@@ -1,9 +1,6 @@
-#from sklearn import preprocessing
-#from sklearn.linear_model import LinearRegression
-#import statsmodels.api as sm
-#import sklearn.metrics
-#from sklearn.model_selection import train_test_split
-#from sklearn.model_selection import KFold
+import sklearn.metrics
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 import xgboost as xgb
 import numpy as np
@@ -13,11 +10,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+from sklearn.model_selection import cross_val_score
 from time import time
-import m2cgen as m2c
-
-
 def min_max_norm(x):
     x = np.array(x)
     x_norm = (x - np.min(x)) / (np.max(x) - np.min(x))
@@ -31,7 +25,6 @@ def test_min_max_norm(x, min, max):
 
 
 def train_feature_matrix(values):
-
     minmax = []
     value, min, max = min_max_norm(values['Judge_Perf'].values)
     minmax.append(min)
@@ -61,10 +54,12 @@ def train_feature_matrix(values):
     minmax.append(min)
     minmax.append(max)
     values['Judge_Ttfb'] = pd.Series(value, index=values['Judge_Ttfb'].index)
-    value, min, max  = min_max_norm(values['Judge_Ttfb_9'].values)
+    value, min, max = min_max_norm(values['Judge_Ttfb_9'].values)
+    print(values['Judge_Ttfb_9'].values)
     minmax.append(min)
     minmax.append(max)
     values['Judge_Ttfb_9'] = pd.Series(value, index=values['Judge_Ttfb_9'].index)
+    print(value)
     value, min, max = min_max_norm(values['Judge_Ttfb_3'].values)
     minmax.append(min)
     minmax.append(max)
@@ -85,8 +80,8 @@ def train_feature_matrix(values):
     feature = values
     return feature, minmax
 
-def test_feature_matrix(values, minmax):
 
+def test_feature_matrix(values, minmax):
     value = test_min_max_norm(values['Judge_Perf'].values, minmax[0], minmax[1])
     values['Judge_Perf'] = pd.Series(value, index=values['Judge_Perf'].index)
     value = test_min_max_norm(values['Judge_Thp'].values, minmax[2], minmax[3])
@@ -115,39 +110,62 @@ def test_feature_matrix(values, minmax):
     return feature
 
 
-def train_tcp(df1):
+def train_tcp(df1, df2):
 
     df1, minmax = train_feature_matrix(
         df1.drop(['BW', 'Loss', 'Delay', 'TCP-Mean', 'QUIC-Mean', 'TCP-SucR', 'QUIC-SucR'], axis=1))
-
-
-    train_feature = df1[['Judge_Retran_long', 'Judge_Ttfb_9', 'Judge_Thp', 'Judge_Rtt_9', 'Judge_Ttfb_rate',
-                   'Judge_Ttfb', 'Judge_Perf']]
+    #print(df1[['Judge_Perf']])
+    train_feature = df1[['Judge_Retran_long', 'Judge_Ttfb_9', 'Judge_Thp', 'Judge_Rtt_9', 'Judge_Ttfb_rate', 'Judge_Ttfb', 'Judge_Perf']]
     train_label = df1['Best']
+
+    df2 = test_feature_matrix(
+        df2.drop(['BW', 'Loss', 'Delay', 'TCP-Mean', 'QUIC-Mean', 'TCP-SucR', 'QUIC-SucR'], axis=1), minmax)
+
+    test_feature = df2[['Judge_Retran_long', 'Judge_Ttfb_9', 'Judge_Thp', 'Judge_Rtt_9', 'Judge_Ttfb_rate', 'Judge_Ttfb', 'Judge_Perf']]
+    test_label = df2['Best']
 
     gbm = xgb.XGBClassifier(learning_rate=0.3, n_estimators=100, max_depth=7, min_child_weight=1, seed=27,
                             subsample=0.9, colsample_bytree=0.8, gamma=0.2, reg_alpha=0, reg_lambda=1, n_jobs=44,
                             eval_metric='merror', objective='binary:logistic')
-
     print("Begin to fit TCP")
     t0 = time()
     gbm.fit(train_feature, train_label)
+    y_score = gbm.predict(test_feature)
     print("Done Fiting in %0.3fs" % (time() - t0))
-    jia = gbm.feature_importances_
-    print(jia)
-    print(gbm.feature_importances_)
-    gbm.save_model('TCP.model')
-    code = m2c.export_to_c(gbm)
-    return code, minmax
+    fi = gbm.feature_importances_
+    print(fi)
+    y_ret = y_score
+    ret = pd.Series(y_ret, index=df2.index)
+
+    confusion_matrix = sklearn.metrics.confusion_matrix(test_label, ret.values)
+    print('confusion_matrix:\n', confusion_matrix)
+
+    precision, recall, fscore, support = sklearn.metrics.precision_recall_fscore_support(
+        test_label, ret.values)
+    print(precision, recall, fscore, support)
+    precision_macro, recall_macro, fscore_macro, support_macro = sklearn.metrics.precision_recall_fscore_support(
+        test_label, ret.values, beta=1, average='macro')
+    fscore_macro = float(2 * precision_macro * recall_macro / (precision_macro + recall_macro))
+    print('macro:', precision_macro, recall_macro, fscore_macro, support_macro)
+
+    return len(y_score), precision_macro, recall_macro, fscore_macro, minmax
 
 
-def train_quic(df1):
+def train_quic(df1, df2):
     df1, minmax = train_feature_matrix(
         df1.drop(['BW', 'Loss', 'Delay', 'TCP-Mean', 'QUIC-Mean', 'TCP-SucR', 'QUIC-SucR'], axis=1))
-    #df1 = df1.reindex(np.random.permutation(df1.index))
 
     train_feature = df1[['Judge_Retran_long', 'Judge_Ttfb_9', 'Judge_Ttfb_rate', 'Judge_Rtt_9', 'Judge_Thp']]
     train_label = df1['Best']
+
+    df2 = test_feature_matrix(
+        df2.drop(['BW', 'Loss', 'Delay', 'TCP-Mean', 'QUIC-Mean', 'TCP-SucR', 'QUIC-SucR'], axis=1), minmax)
+
+    test_feature = df2[['Judge_Retran_long', 'Judge_Ttfb_9', 'Judge_Ttfb_rate', 'Judge_Rtt_9', 'Judge_Thp']]
+    test_label = df2['Best']
+
+    # gbm = xgb.XGBClassifier(learning_rate=0.3, n_estimators=400, n_jobs=44, eval_metric='merror',
+    #                        objective='binary:logistic')
 
     gbm = xgb.XGBClassifier(learning_rate=0.3, n_estimators=200, max_depth=7, min_child_weight=1, seed=27,
                             subsample=0.9, colsample_bytree=0.8, gamma=0.3, reg_alpha=0, reg_lambda=1, n_jobs=44,
@@ -155,55 +173,58 @@ def train_quic(df1):
     print("Begin to fit QUIC")
     t0 = time()
     gbm.fit(train_feature, train_label)
+    y_score = gbm.predict(test_feature)
     print("Done Fiting in %0.3fs" % (time() - t0))
-    print(gbm.feature_importances_)
-    gbm.save_model('QUIC.model')
-    code = m2c.export_to_c(gbm)
-    return code, minmax
+    fi = gbm.feature_importances_
+    print(fi)
+    y_ret = y_score
+    ret = pd.Series(y_ret, index=df2.index)
+    # ret = ret.reindex(test_data.index)
+    confusion_matrix = sklearn.metrics.confusion_matrix(test_label, ret.values)
+    print('confusion_matrix:\n', confusion_matrix)
 
+    precision, recall, fscore, support = sklearn.metrics.precision_recall_fscore_support(
+        test_label, ret.values)
+    print(precision, recall, fscore, support)
+    precision_macro, recall_macro, fscore_macro, support_macro = sklearn.metrics.precision_recall_fscore_support(
+        test_label, ret.values, beta=1, average='macro')
+    fscore_macro = float(2 * precision_macro * recall_macro / (precision_macro + recall_macro))
+    print('macro:', precision_macro, recall_macro, fscore_macro, support_macro)
+    return len(y_score), precision_macro, recall_macro, fscore_macro, minmax
 
 def train(filename1):
     df1 = pd.read_csv(filename1, index_col=None)
     df1 = df1.reindex(np.random.permutation(df1.index))
+    df12, df3 = train_test_split(df1, test_size=0.333, random_state=0)
+    df1 = df12
+    df2 = df3
 
-
-    df1_tcp = df1.loc[df1.Protocol == 0, ["BW", "Loss", "Delay", "Window", "Timestamp", "Wise", "Method", "NetType", "OsType", "Status", "Quic_alter_race",
+    feature_name = ["BW", "Loss", "Delay", "Window", "Timestamp", "Wise", "Method", "NetType", "OsType", "Status", "Quic_alter_race",
          "Is_zero_rtt", "Received_bytes", "Sent_bytes", "Protocol", "Weak", "Duration", "Ttfb", "Ttfb_avg", "Judge_Ttfb",
          "Judge_Ttfb_9", "Judge_Ttfb_3", "Ttfb_rate", "Judge_Ttfb_rate", "Rtt", "Judge_Rtt_recent", "Judge_Rtt_9",
          "Judge_Rtt_3", "Judge_Rtt_rate", "Retran", "Judge_Retran_short", "Judge_Retran_long", "Throughput", "Performance",
          "Judge_Thp", "Judge_Perf", "Sends", "InitCon", "Udp", "Body_recv", "Connection Time", "Transfer Time", "Dns",
          "Redirect", "Pending", "Head_recv", "Ssl", "SinChange", "TcpCon", "SinEnStart", "TCP-Mean", "QUIC-Mean",
-         "TCP-Med", "QUIC-Med", "TCP-SucR", "QUIC-SucR", "Best"]]#0:57]
-    df1_quic = df1.loc[df1.Protocol == 1, ["BW", "Loss", "Delay", "Window", "Timestamp", "Wise", "Method", "NetType", "OsType", "Status", "Quic_alter_race",
-         "Is_zero_rtt", "Received_bytes", "Sent_bytes", "Protocol", "Weak", "Duration", "Ttfb", "Ttfb_avg", "Judge_Ttfb",
-         "Judge_Ttfb_9", "Judge_Ttfb_3", "Ttfb_rate", "Judge_Ttfb_rate", "Rtt", "Judge_Rtt_recent", "Judge_Rtt_9",
-         "Judge_Rtt_3", "Judge_Rtt_rate", "Retran", "Judge_Retran_short", "Judge_Retran_long", "Throughput", "Performance",
-         "Judge_Thp", "Judge_Perf", "Sends", "InitCon", "Udp", "Body_recv", "Connection Time", "Transfer Time", "Dns",
-         "Redirect", "Pending", "Head_recv", "Ssl", "SinChange", "TcpCon", "SinEnStart", "TCP-Mean", "QUIC-Mean",
-         "TCP-Med", "QUIC-Med", "TCP-SucR", "QUIC-SucR", "Best"]]#0:57]
+         "TCP-Med", "QUIC-Med", "TCP-SucR", "QUIC-SucR", "Best"]
+    df1_tcp = df1.loc[df1.Protocol == 0, feature_name]
+    df1_quic = df1.loc[df1.Protocol == 1, feature_name]
 
-    t_code, t_minmax = train_tcp(df1_tcp)
-    q_code, q_minmax = train_quic(df1_quic)
+    df2_tcp = df2.loc[df2.Protocol == 0, feature_name]
+    df2_quic = df2.loc[df2.Protocol == 1, feature_name]
 
-    ft = open('tcp_minmax.txt','w')
-    print(t_minmax)
-    for t in t_minmax:
-        ft.write(str(t)+', ')
-    ft.write('\n')
-    ft.close()
-    ft = open('tcp_code_all.txt', 'w')
-    ft.write(t_code)
-    ft.close()
-    fq = open('quic_minmax.txt','w')
-    for q in q_minmax:
-        fq.write(str(q)+', ')
-    fq.write('\n')
-    fq = open('quic_code_all.txt', 'w')
-    fq.write(q_code)
-    fq.close()
+    tcplen, t_p, t_r, t_f, t_minmax = train_tcp(df1_tcp, df2_tcp)
+    quiclen, q_p, q_r, q_f, q_minmax = train_quic(df1_quic, df2_quic)
+
+    print("******************ALL************")
+    precision_macro = (t_p * tcplen + q_p *quiclen ) / (tcplen + quiclen)
+    recall_macro = (t_r * tcplen + q_r *quiclen ) / (tcplen + quiclen)
+    fscore_macro = float(2 * precision_macro * recall_macro / (precision_macro + recall_macro))
+    print('macro:', precision_macro, recall_macro, fscore_macro)
+
 
 if __name__ == '__main__':
     train("Train_data.csv")
+
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WiseTrans
-Adaptive Transport Protocol Selection for Mobile Web Service (TheWebConf 2021)
+WiseTrans is an adaptive transport protocol selection for mobile web service. See the paper for more details.
+
+## Offline Training
+WiseTrans collects logs on testbed with various network conditions for offline training.  
+The traces and running scripts for training could be found in `<OfflineTraining>`.
+
+## Online Selection
+WiseTrans selects and switches transport protocols at the request level to reach both high performance and acceptable overhead.  
+WiseTrans is implemented in Baidu's network library. We only provide sample scripts (for Android) to show the process how WiseTrans extracts features and selects protocol in `<OnlineSelection>`.
+
+## Measurements of one popular mobile web service in the wild
+We conduct large-scale passive measurements of one popular mobile app of Baidu. Users can access the short video mobile web service of Baidu with this app. We collect and sample feed refresh request data from the users, which is sent by the app when a user wants to refresh the current list of recommended videos.  
+The request logs could be found in `<Measurements>`.  
+It is worth mentioning that the analysis of user's location and ISP in our paper is based on user's IP addresses. For ethical and privacy considerations, we have hidden the ip address as well as other private imformation in the logs. Therefore, readers may not be able to obtain these two information. Information related to the status of the network and the transport process of the request is still available.
+
+For any questions, please post an issue or send an email to [joycezhangjia@gmail.com](mailto:joycezhangjia@gmail.com).
+
